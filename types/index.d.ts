eee0683df77733871df47ec359b2af25c6a3ac58
EGOIST <0x142857@gmail.com>
2018-04-17 14:35:27 +0800

    add d.ts

---
+++ types/index.d.ts
@@ -0,0 +1,46 @@
+export interface Options {
+  /* a list of files to search */
+  files?: string[]
+  /* the directory to search from */
+  cwd?: string
+  /* the directory to stop searching */
+  stopDir?: string
+}
+
+export interface LoadResult {
+  /* file path */
+  path?: string
+  /* file data */
+  data?: any
+}
+
+export interface AsyncLoader {
+  test: RegExp
+  load(filepath: string): Promise<any>
+}
+
+export interface SyncLoader {
+  test: RegExp
+  loadSync(filepath: string): any
+}
+
+export interface MultiLoader {
+  test: RegExp
+  load(filepath: string): Promise<any>
+  loadSync(filepath: string): any
+}
+
+declare class JoyCon {
+  constructor(options: Options)
+
+  resolve(files?: string[], cwd?: string, stopDir?: string): Promise<string | null>
+  resolveSync(files?: string[], cwd?: string, stopDir?: string): string | null
+
+  load(files?: string[], cwd?: string, stopDir?: string): Promise<LoadResult>
+  loadSync(files?: string[], cwd?: string, stopDir?: string): LoadResult
+
+  addLoader(loader: AsyncLoader | SyncLoader | MultiLoader): this
+}
+
+
+export default JoyCon
---
+++ types/test.ts
@@ -0,0 +1,25 @@
+import JoyCon from '..'
+
+const joycon = new JoyCon({
+  files: ['foo.js']
+})
+
+joycon.resolve()
+.then(path => {
+  console.log(path)
+})
+
+joycon.load()
+.then(res => {
+  console.log(res.path)
+  console.log(res.data)
+})
+
+joycon.loadSync()
+
+joycon.addLoader({
+  test: /\.ts$/,
+  loadSync(fp) {
+    return 123
+  }
+})
